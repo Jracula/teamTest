135808b122d2da37e9d833f60542ac9d63c9a477
growgi <growgi@users.noreply.github.com>
2023-03-09 19:35:21 +0900

    접근권한에 따라 페이지 제한시킴
    비회원: 로그인 전용기능 불가
    회원: 본인 계정만 가능. 타인 계정 접근불가

--- gitTestProject/src/main/java/com/litbooks/member/controller/selectAllRecommServlet.java
+++ gitTestProject/src/main/java/com/litbooks/member/controller/selectAllRecommServlet.java
@@ -8,10 +8,12 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import com.litbooks.book.vo.BookView;
 import com.litbooks.member.sevice.MemberService;
 import com.litbooks.member.vo.AllMemberData;
+import com.litbooks.member.vo.Member;
 
 /**
  * Servlet implementation class selectAllRecommServlet
@@ -34,7 +36,24 @@ public class selectAllRecommServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		//인코딩
     	request.setCharacterEncoding("utf-8");
-		
+
+	//관리자 로그인인지 확인하기 위해서 현재 세션의 정보를 일단 받음
+		HttpSession session = request.getSession(false);
+		Member me = (Member) session.getAttribute("m");
+	//비로그인자는 접근 거부하는 if문
+		if (me == null) {
+			RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp");
+			request.setAttribute("title", "접근 제한");
+			request.setAttribute("msg", "비회원은 접근 불가능합니다.");
+			request.setAttribute("icon", "error");
+			request.setAttribute("loc", "/index.jsp");
+			view.forward(request, response);
+		} else {
+	//관리자가 맞으면 정상 실행
+			if (me.getMemberLevel() == 1) {
+//servlet 작성부 시작
+
+
 		//값추출
 		int reqPage = Integer.parseInt(request.getParameter("reqPage"));
 
@@ -48,6 +67,19 @@ public class selectAllRecommServlet extends HttpServlet {
 		request.setAttribute("pageNavi", bv.getPageNavi());
 		request.setAttribute("start", bv.getStart());
 	    view.forward(request, response);
+
+
+//servlet 작성부 끝
+	//관리자가 아닌 로그인일 때 거부
+			} else {
+				RequestDispatcher view = request.getRequestDispatcher("/WEB-INF/views/common/msg.jsp");
+				request.setAttribute("title", "접근 제한");
+				request.setAttribute("msg", "관리자 전용 기능입니다.");
+				request.setAttribute("icon", "error");
+				request.setAttribute("loc", "/index.jsp");
+				view.forward(request, response);
+			}
+		}
 	}
 
 	/**
